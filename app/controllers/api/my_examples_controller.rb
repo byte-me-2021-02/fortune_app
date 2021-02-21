af94fa5149da3e7d05973f0b86b968059084f1f2
brian <riceb53@gmail.com>
2021-02-21 13:18:03 -0500

    show random fortune

--- app/controllers/api/my_examples_controller.rb
+++ app/controllers/api/my_examples_controller.rb
@@ -1,5 +1,10 @@
 class Api::MyExamplesController < ApplicationController
   def fortune
+    fortunes = ["you will be nice", "you will be happy", "you will sail the seven seas"]
+
+    # p "HELLOOOOOOO"
+    @selected_fortune = fortunes.sample
+    
     render 'the_fortune.json.jb'
   end
 end
